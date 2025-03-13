b79e59625b611aa2d487928c81b75dedc13528cb
SpursFan21 <duncanfrederic@gmail.com>
2025-03-13 17:27:03 +1300

    updated camping page

--- src/app/camping/page.tsx
+++ src/app/camping/page.tsx
@@ -49,10 +49,10 @@ export default function CampingPage() {
         <h2 className="text-4xl font-bold mb-4">Camping Information</h2>
         <div className="text-lg mb-4 space-y-4 text-left">
           <p>
-            Car camping is available. Your vehicle will be parked adjacent to your designated camping space, with a 6–7 foot buffer between vehicles and 10–20 yards of clearance perpendicular for tents, pop-ups, or other camping setups.
+            Car camping is available. Your vehicle will be parked adjacent to your designated camping space, with a 6–7 foot buffer between vehicles and 10–20 yards of clearance perpendicular for tents, pop-ups, or other setups.
           </p>
           <p>
-            Quiet hours will be observed from 4:00 AM to 9:00 AM. We kindly ask that all campers respect their neighbors by keeping noise to a minimum during this period.
+            Quiet hours will be observed from 4:00 AM to 9:00 AM. We kindly ask all campers to respect their neighbors by keeping noise to a minimum during this period.
           </p>
           <p>
             Portable toilets will be provided on-site, and medical and security staff will be available at all times for your safety.
@@ -72,8 +72,33 @@ export default function CampingPage() {
         </div>
       </div>
 
+      {/* San Luis Camping List */}
+      <div className="mx-auto p-8 text-center max-w-3xl">
+        <h2 className="text-4xl font-bold mb-4">San Luis Camping List</h2>
+        <ul className="list-disc list-inside text-lg text-left space-y-2">
+          <li>Toiletries and Baby Wipes</li>
+          <li>Tent or Car for sleeping quarters</li>
+          <li>
+            Waterproofing bags and tarps for vending/personal items (lesson learned from last year)
+          </li>
+          <li>
+            Water (We will have water available, but please bring extra for yourself and friends!)
+          </li>
+          <li>Food (snacks and extra sustenance)</li>
+          <li>Wind Breaker (it gets windy)</li>
+          <li>Bug repellant (AN ABSOLUTE NECESSITY)</li>
+          <li>Sunscreen</li>
+          <li>Good Vibes</li>
+          <li>
+            Responsible Mindset (reckless behavior can lead to issues; we’re celebrating freedom responsibly)
+          </li>
+          <li>Even though there will be portapotties, please bring your own toilet paper.</li>
+        </ul>
+      </div>
+
       {/* Spacer */}
-      <div className="my-16"></div>
+      <div className="my-20"></div>
+
 
       {/* Footer */}
       <Footer />
